3aca35d31eddd57f626524ea65f7e7e47a8f6827
pmrvic <mrvic.predrag@gmail.com>
2019-02-20 22:07:30 +0100

    DOdan projekt zbroj

---
+++ Zbroj/Zbroj/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zbroj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("program 2");
+        }
+    }
+}
