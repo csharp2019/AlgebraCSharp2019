c11a5bc51d8c92d51bc5122917aabb72b0e0001f
pmrvic <mrvic.predrag@gmail.com>
2019-02-21 18:54:41 +0100

    nastavak slijedi

--- ConsoleApp1/Zbroj/Program.cs
+++ ConsoleApp1/Zbroj/Program.cs
@@ -10,6 +10,20 @@ namespace Zbroj
     {
         static void Main(string[] args)
         {
+            // deklaracija vraijabli
+            int x, y;   
+
+            // definicija varijabli x i y
+            x = 5;
+            y = x + 3;
+
+            // Ispis u konzolu
+            Console.WriteLine("broj x je:" + x);
+            Console.WriteLine("broj y je:" + y);
+            Console.WriteLine("Njihov zbroj je:" + (x+y));
+
+            // Ne zatvaraj dok se ne stisne key
+            Console.ReadKey();
         }
     }
 }
