599570b2e8a014b084db07d1b36fb6c494e393dc
pmrvic <mrvic.predrag@gmail.com>
2019-02-22 17:39:52 +0100

    dodano djeljenje i umnožak

---
+++ ConsoleApp1/zbroj_razlika/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zbroj_razlika
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=0, b = 0;
+
+            Console.WriteLine("Unesi 1. broj");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Unesi 2. broj");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Zbroj je:{0}", a + b);
+
+            Console.WriteLine("Razlika je:{0}", a - b);
+
+            Console.WriteLine("Razlika je:{1} Zbroj je:{0}", a + b, a - b);
+
+            Console.WriteLine("Umnožak je:{0}", a * b);
+
+            Console.WriteLine("Kvocijent je:{0}", (decimal)a/(decimal)b);
+
+            Console.ReadLine();
+        }
+    }
+}
