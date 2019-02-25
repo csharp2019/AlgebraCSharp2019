fce3cd5cc023d8227f55c8ef337919a0ff82e2dd
pmrvic <mrvic.predrag@gmail.com>
2019-02-25 17:38:11 +0100

    exception primjer

--- ConsoleApp1/5.2.3.exceptio_primjer/Program.cs
+++ ConsoleApp1/5.2.3.exceptio_primjer/Program.cs
@@ -10,6 +10,39 @@ namespace _5._2._3.exceptio_primjer
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Console.Write("Unesite cijeli broj: ");
+                int a = int.Parse(Console.ReadLine());
+
+                if (a > 0)
+                {
+                    Console.WriteLine("Uneseni broj je pozitivan");
+                }
+                else if (a < 0)
+                {
+                    Console.WriteLine("Uneseni broj je negativan");
+                }
+                else
+                {
+                    Console.WriteLine("Uneseni broj je jednak nuli");
+                }
+
+
+            }
+            catch(FormatException fex)
+            {
+                Console.WriteLine("GREŠKA, pogrešan format:" 
+                    + fex.Message
+                    + "\n"
+                    + fex.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("GREŠKA, generička:" + ex.Message);
+            }
+            Console.ReadKey();
         }
     }
 }
+
