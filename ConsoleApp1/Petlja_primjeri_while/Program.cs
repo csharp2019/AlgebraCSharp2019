e32af3a7377e4dd1f75157c3e71cf9b585524845
pmrvic <mrvic.predrag@gmail.com>
2019-02-26 18:11:46 +0100

    primjer petlja for

---
+++ ConsoleApp1/Petlja_primjeri_while/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petlja_primjeri_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ime = "";
+            while (ime != "kraj")
+            {
+
+            }
+        }
+    }
+}
