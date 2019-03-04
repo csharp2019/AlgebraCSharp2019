43c3ae315f061004862884822864378528615ce4
pmrvic <mrvic.predrag@gmail.com>
2019-03-04 17:12:10 +0100

    zadaca 5.3

---
+++ ConsoleApp1/5.3.18_unos_ocjene/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_3_18_unos_cijene
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                int a = 0;
+                bool unosOK = false;
+
+
+                while (!unosOK)
+                {
+                    Console.WriteLine("Unesite vašu ocjenu [1,5]: ");
+                    a = int.Parse(Console.ReadLine());
+
+                    if (a == 1 || a == 2 || a == 3 || a == 4 || a == 5)
+                    {
+                        unosOK = true;
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Ocjena krivo unešena!");
+                    }
+                }
+
+                Console.WriteLine("\n -hvala najljepša...");
+            }
+
+            catch (FormatException fex)
+            {
+                Console.WriteLine("Ponovno pokrenite program i unesite cijeli prirodni broj! \n\n\n" + fex.ToString());
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/5.3.22_kalkulator_2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak_5._3._22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a = 0, b = 0;
+            string operacija = "";
+            Char odgovor = 'd';
+
+            while (odgovor == 'd')
+            {
+                Console.WriteLine("Unesite prvi broj: ");
+                a = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("Unesite drugi broj: ");
+                b = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("Unesite računsku operaciju (+,-,*,/): ");
+                operacija = Console.ReadLine();
+
+                switch (operacija)
+                {
+                    case "+":
+                        Console.WriteLine("Zbroj: {0}", a + b);
+                        break;
+                    case "-":
+                        Console.WriteLine("Razlika: {0}", a - b);
+                        break;
+                    case "*":
+                        Console.WriteLine("Umnožak: {0}", a * b);
+                        break;
+                    case "/":
+                        Console.WriteLine("Kvocijent: {0}", a / b);
+                        break;
+                    default:
+                        Console.WriteLine("Nepoznata računska operacija!");
+                        break;
+                }
+
+                Console.WriteLine("Nastaviti računati? (D/N)");
+                odgovor = Char.ToLower(Console.ReadKey().KeyChar);
+                if (odgovor == 'n')
+                {
+                    break;
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
