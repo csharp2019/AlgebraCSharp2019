50a841e831e6926e9ea5196a826baaf1bd9350fe
pmrvic <mrvic.predrag@gmail.com>
2019-03-11 17:20:49 +0100

    Unit testing

---
+++ ConsoleApp1/8.2.1_static/Static.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8._2._1_static
+{
+    public class Static
+
+    {
+        private Static() { }
+
+        public static double Zbroj(double a, double b)
+        {
+            return a + b;
+        }
+        public static double Kub(double broj)
+        {
+            return broj * broj * broj;
+        }
+        public static double Udaljenost(double x1, double y1, double x2, double y2)
+        {
+            return Math.Sqrt(((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)));
+        }
+        public static double Fahrenheit(double stupnjevaCelzijusa)
+        {
+            return ((double)9 / 5) * stupnjevaCelzijusa + 32;
+        }
+    }
+}
---
+++ ConsoleApp1/8.2.1_staticTests/StaticTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _8._2._1_static;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8._2._1_static.Tests
+{
+    [TestClass()]
+    public class StaticTests
+    {
+        [TestMethod()]
+        public void ZbrojTest()
+        {
+            Assert.AreEqual(6, Static.Zbroj(3.4, 2.6));
+            //Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void KubTest()
+        {
+            Assert.AreEqual(8, Static.Kub(2));
+        }
+
+        [TestMethod()]
+        public void UdaljenostTest()
+        {
+            Assert.AreEqual(2, Static.Udaljenost(0,0,0,2));
+            Assert.AreEqual((199.740063125653).ToString("F6"), (Static.Udaljenost(0.0005, 0.26, 0.1593, 200)).ToString("F6"));
+            Assert.AreNotEqual(3, Static.Udaljenost(0, 0, 0, 2));
+
+        }
+
+        [TestMethod()]
+        public void FahrenheitTest()
+        {
+            Assert.AreEqual(60.8, Static.Fahrenheit(16));
+            Assert.AreEqual(32, Static.Fahrenheit(0));
+            Assert.AreEqual(23, Static.Fahrenheit(-5));
+            Assert.AreEqual(-508, Static.Fahrenheit(-300));
+            Assert.AreEqual(-5368, Static.Fahrenheit(-3000));
+            Assert.IsTrue(Static.Fahrenheit(-150) >= -273);
+            
+
+
+        }
+    }
+}
