7099f260c4c1454ca17172de375d696c3d73b75a
pmrvic <mrvic.predrag@gmail.com>
2019-03-13 18:13:58 +0100

    eventi

---
+++ ConsoleApp1/8.5.1_eventi/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8._5._1_eventi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Osoba osoba = new Osoba();
+            osoba.NaPromjenuImena += new
+                Osoba.NaPromjenuImenaDelegat(osoba_NaPromjenuImena);
+            osoba.Ime = "Pero";
+
+            osoba.NaPromjenuImena -= new
+                Osoba.NaPromjenuImenaDelegat(osoba_NaPromjenuImena);
+
+            Console.ReadKey();
+        }
+
+        static void osoba_NaPromjenuImena(object sender, EventArgs e)
+        {
+            Console.WriteLine("Osoba je promjenila podatke: {0}",
+                ((Osoba)sender).Ime);
+        }
+    }
+}
